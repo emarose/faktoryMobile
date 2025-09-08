e5c15ec269c5f9093abe154fed9400f7112cb187
Emanuel Amerio <83380305+emarose@users.noreply.github.com>
2025-09-07 22:59:15 -0300

    agrego archivo de constantes}

---
+++ src/constants/Consts.js
@@ -0,0 +1,6 @@
+export const FONT_SIZE_SMALL = 12;
+export const FONT_SIZE_MEDIUM = 16;
+export const FONT_SIZE_LARGE = 20;
+export const BORDER_RADIUS = 8;
+export const PADDING = 10;
+export const MARGIN = 10;
